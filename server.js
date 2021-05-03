0e0cb1c32d73966a6533de4abf50d6130290f362
DianaTrujillo-create <diana.trujillo0806@gmail.com>
2021-05-03 07:03:20 -0500

    commit to main

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
+require("dotenv").config;
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const { MongoServerSelectionError } = require("mongodb");
 
-const app = express();
 const PORT = process.env.PORT || 3000;
 
 const app = express();
